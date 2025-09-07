839466862e5f68981aa13fe9d4634cad9e8c820e
Yuriy Polosov <ypolosov@gmail.com>
2025-09-07 15:47:21 +0000

    fix

--- check-kafka.sh
+++ check-kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9092,localhost:9093,localhost:909
+~/kafka/bin/kafka-broker-api-versions.sh --bootstrap-server localhost:9092,localhost:9093,localhost:9094
